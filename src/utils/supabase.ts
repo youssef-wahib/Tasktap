a72102f50e217aeced7393bb376964af0e8f8c3f
youssef-wahib <youssefwahib32@gmail.com>
2024-05-11 16:32:30 +0300

    Project Refactored

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 import { Database } from "../supabaseTypes.ts";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.VITE_APPKEY as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
